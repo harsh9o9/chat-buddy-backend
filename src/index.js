6c7da4f0c6fa3cd1ef2052974a4fe867326e8042
harsh9o9 <hbhardwaj454@gmail.com>
2024-01-08 00:13:13 +0530

    trying to fix secretOrPrivateKey .env file error in prod

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 import httpServer from "./server.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({
-  path: "./.env",
-});
-
+// dotenv.config({
+//   path: "./.env",
+// });
 const startServer = () => {
   httpServer.listen(process.env.PORT || 8080, () => {
     console.log("⚙️  Server is running on port: " + process.env.PORT || 8080);
